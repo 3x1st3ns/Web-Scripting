961186a5bbdd36371cb9536b2da9f7fb5b5e5700
3x1st3ns <danizspain82@gmail.com>
2021-07-12 09:36:28 +0200

    Simple

--- scripting.py
+++ scripting.py
@@ -3,19 +3,23 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import os
+import sys
 
-os.system("clear")
-print("\n")
-print("******************************** WebScripting [Started] ***********************************")
-print("\n")
-web = input("Put the website [https://www.website.com/] :")
-print("\n")
-data = urllib.request.urlopen(web).read().decode()
-print(data)
+if len(sys.argv) != 2:
+    print("\n[!] Uso: python3 " + sys.argv[0] + " <https://example.com>\n")
+    sys.exit(1)
 
-soup = BeautifulSoup(data)
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href'))
 
-print("\n****************************** WebScripting [Finished] *************************************")
+if __name__ == "__main__":
+  try:
+    print("\n")
+    web = sys.argv[1]
+    data = urllib.request.urlopen(sys.argv[1]).read().decode()
+
+    soup = BeautifulSoup(data)
+    tags = soup('a')
+    for tag in tags:
+      print(tag.get('href'))
+      print("\n")
+  except: 
+    print("[!] Please write good the URL <https://www.example.com>\n")
